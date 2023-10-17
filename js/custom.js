560b4993e8af8adaecfad2240516a8a10069dfc7
Muchai248 <muchaimark6@gmail.com>
2023-10-17 09:57:57 +0300

    added

--- js/custom.js
+++ js/custom.js
@@ -9,34 +9,34 @@ getYear();
 
 /** google_map js **/
 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// function myMap() {
+//     var mapProp = {
+//         center: new google.maps.LatLng(40.712775, -74.005973),
+//         zoom: 18,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
 
-// Get references to the image element and the container
-const image = document.getElementById('rotate-image');
-const container = document.getElementById('container');
+// // Get references to the image element and the container
+// const image = document.getElementById('rotate-image');
+// const container = document.getElementById('container');
 
-// Function to update the image's perspective-based rotation
-function rotateImage(event) {
-  // Calculate the rotation angles based on the mouse position
-  const containerRect = container.getBoundingClientRect();
-  const x = event.clientX - containerRect.left - containerRect.width / 2;
-  const y = event.clientY - containerRect.top - containerRect.height / 2;
-  const rotationX = -y / containerRect.height * 360; // Adjust the factor as needed
-  const rotationY = x / containerRect.width * 360; // Adjust the factor as needed
+// // Function to update the image's perspective-based rotation
+// function rotateImage(event) {
+//   // Calculate the rotation angles based on the mouse position
+//   const containerRect = container.getBoundingClientRect();
+//   const x = event.clientX - containerRect.left - containerRect.width / 2;
+//   const y = event.clientY - containerRect.top - containerRect.height / 2;
+//   const rotationX = -y / containerRect.height * 360; // Adjust the factor as needed
+//   const rotationY = x / containerRect.width * 360; // Adjust the factor as needed
 
-  // Apply the rotation transform to the image with perspective
-  image.style.transform = `perspective(800px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
-}
+//   // Apply the rotation transform to the image with perspective
+//   image.style.transform = `perspective(800px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+// }
 
 
-// Add a mousemove event listener to the container
-container.addEventListener('mousemove', rotateImage);
+// // Add a mousemove event listener to the container
+// container.addEventListener('mousemove', rotateImage);
 
 
 // Get a reference to the new image element and its container
@@ -88,32 +88,41 @@ window.addEventListener('scroll', () => {
 //   const targetElement = document.querySelector('h3');
 //   targetElement.scrollIntoView({ behavior: 'smooth' });
 // }
-
 const canvas = document.querySelector(".canvas");
 const context = canvas.getContext("2d");
 const frameCount = 130;
 const images = [];
+let imageLoadCount = 0; // Track the number of loaded images
+const body = { frame: 0 };
+// Function to handle image loading
+function onImageLoad() {
+    imageLoadCount++;
+
+    // Check if all images are loaded
+    if (imageLoadCount === frameCount) {
+        // All images are loaded, now you can start rendering
+        
+        gsap.to(body, {
+            frame: frameCount - 1,
+            snap: "frame",
+            ease: "none",
+            scrollTrigger: {
+                scrub: 1,
+                pin: ".canvas",
+                end: "100%",
+            },
+            onUpdate: render,
+        });
+    }
+}
 
 // Preload all images
 for (let i = 1; i <= frameCount; i++) {
     const img = new Image();
+    img.onload = onImageLoad; // Set the load event handler
     img.src = `./slides/${i.toString().padStart(4, '0')}.png`;
     images.push(img);
 }
-
-const body = { frame: 0 };
-
-gsap.to(body, {
-    frame: frameCount - 1,
-    snap: "frame",
-    ease: "none",
-    scrollTrigger: {
-        scrub: 1,
-        pin: ".canvas",
-        end: "100%",
-    },
-    onUpdate: render,
-});
 
 function render() {
     context.canvas.width = images[0].width;
